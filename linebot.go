7ce4a3df67051757f025cfd2f8247f2c5f1f1530
jielite <ita3051@gmail.com>
2016-11-12 04:48:11 +0800

    echo

--- linebot.go
+++ linebot.go
@@ -1,39 +1,54 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
-type Configuration struct {
-	CHANNEL_SECRET       string
-	CHANNEL_ACCESS_TOKEN string
-}
-
-func loadConfigFile(fPath string) *Configuration {
-	configuration := Configuration{}
-
-	file, _ := os.Open(fPath)
-	defer file.Close()
-
-	decoder := json.NewDecoder(file)
-	err := decoder.Decode(&configuration)
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-
-	return &configuration
-}
+var bot *linebot.Client
 
 func main() {
-
 	// load configuration
-	botConfigs := loadConfigFile("./bot_settings.json")
-	client := &http.Client{}
-	bot, err := linebot.New(botConfigs.CHANNEL_SECRET, botConfigs.CHANNEL_ACCESS_TOKEN, linebot.WithHTTPClient(client))
-	fmt.Println(client)
+	var err error
+	bot, err = linebot.New(os.Getenv("CHANNEL_SECRET"), os.Getenv("CHANNEL_TOKEN"))
+	if err != nil {
+		log.Println("Bot Initial Error:", err)
+	}
+
+	http.HandleFunc("/", hello)
+	http.HandleFunc("/callback", callBack)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello"))
+	w.WriteHeader(200)
+}
+
+func callBack(w http.ResponseWriter, req *http.Request) {
+	events, err := bot.ParseRequest(req)
+
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	}
 }
